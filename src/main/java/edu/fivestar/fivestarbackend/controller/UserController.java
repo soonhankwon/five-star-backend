00243786dd2333e03451bf481f7625ab87025581
soonhankwon <soonable@gmail.com>
2023-07-11 20:08:57 +0900

    feat : UserController, UserService 기본 세팅

---
+++ src/main/java/edu/fivestar/fivestarbackend/controller/UserController.java
@@ -0,0 +1,7 @@
+package edu.fivestar.fivestarbackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ src/main/java/edu/fivestar/fivestarbackend/service/UserService.java
@@ -0,0 +1,7 @@
+package edu.fivestar.fivestarbackend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}

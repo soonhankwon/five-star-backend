a3ae93b325ed2f23df79675ad63f3c3a35522b2a
soonhankwon <soonable@gmail.com>
2023-07-11 15:52:50 +0900

    feat : util

--- src/main/java/edu/fivestar/fivestarbackend/util/BaseTimeEntity.java
+++ src/main/java/edu/fivestar/fivestarbackend/util/BaseTimeEntity.java
@@ -1,2 +1,9 @@
-package edu.fivestar.fivestarbackend.util;public class BaseTimeEntity {
+package edu.fivestar.fivestarbackend.util;
+
+import java.time.LocalDateTime;
+
+public abstract class BaseTimeEntity {
+
+    private LocalDateTime created_at;
+    private LocalDateTime modifed_at;
 }

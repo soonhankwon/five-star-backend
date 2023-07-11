ad5aaf8aad355139b7085748c879a7121b1c0551
soonhankwon <soonable@gmail.com>
2023-07-11 15:52:11 +0900

    feat : domain

--- src/main/java/edu/fivestar/fivestarbackend/domain/User.java
+++ src/main/java/edu/fivestar/fivestarbackend/domain/User.java
@@ -1,2 +1,24 @@
-package edu.fivestar.fivestarbackend.domain;public class User {
+package edu.fivestar.fivestarbackend.domain;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "`user`")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }

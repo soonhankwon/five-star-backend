47d8dd3214125d69293bcb8fa004002fb73c4360
soonhankwon <soonable@gmail.com>
2023-07-11 16:16:53 +0900

    feat : name 멤버 변수 추가

--- src/main/java/edu/fivestar/fivestarbackend/domain/User.java
+++ src/main/java/edu/fivestar/fivestarbackend/domain/User.java
@@ -15,10 +15,13 @@ public class User {
 
     private String email;
 
+    private String name;
+
     private String password;
 
-    public User(String email, String password) {
+    public User(String email, String password, String name) {
         this.email = email;
         this.password = password;
+        this.name = name;
     }
 }

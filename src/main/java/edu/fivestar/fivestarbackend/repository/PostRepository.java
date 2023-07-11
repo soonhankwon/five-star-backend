02cca4beb5dc89bde57322e8d56fd9d7ca20c498
soonhankwon <soonable@gmail.com>
2023-07-11 15:52:37 +0900

    feat : repository

--- src/main/java/edu/fivestar/fivestarbackend/repository/PostRepository.java
+++ src/main/java/edu/fivestar/fivestarbackend/repository/PostRepository.java
@@ -1,2 +1,7 @@
-package edu.fivestar.fivestarbackend.repository;public class PostRepository {
+package edu.fivestar.fivestarbackend.repository;
+
+import edu.fivestar.fivestarbackend.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Long, Post> {
 }
--- src/main/java/edu/fivestar/fivestarbackend/repository/UserRepository.java
+++ src/main/java/edu/fivestar/fivestarbackend/repository/UserRepository.java
@@ -1,2 +1,7 @@
-package edu.fivestar.fivestarbackend.repository;public interface UserRepository {
+package edu.fivestar.fivestarbackend.repository;
+
+import edu.fivestar.fivestarbackend.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Long, User> {
 }

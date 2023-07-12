0f83fe5e9a5767eef9baf510555fe0fb3a67e6a3
soonhankwon <soonable@gmail.com>
2023-07-12 12:29:34 +0900

    feat : BaseTimeEntity, EntityListener

--- src/main/java/edu/fivestar/fivestarbackend/util/BaseTimeEntity.java
+++ src/main/java/edu/fivestar/fivestarbackend/util/BaseTimeEntity.java
@@ -1,9 +1,25 @@
 package edu.fivestar.fivestarbackend.util;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+@Getter
+@MappedSuperclass
+@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+@EntityListeners(value = AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
+    @CreatedDate
     private LocalDateTime created_at;
-    private LocalDateTime modifed_at;
+
+    @LastModifiedDate
+    private LocalDateTime modified_at;
 }

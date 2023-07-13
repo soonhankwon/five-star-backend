2d51891577d2394ea67d08ce39f20b6ade1ce628
soonhankwon <soonable@gmail.com>
2023-07-13 15:54:05 +0900

    feat : 포스트 생성 req dto 변수 삭제 & 스웨거 설명추가

--- src/main/java/edu/fivestar/fivestarbackend/dto/PostCreateReqDto.java
+++ src/main/java/edu/fivestar/fivestarbackend/dto/PostCreateReqDto.java
@@ -1,13 +1,17 @@
 package edu.fivestar.fivestarbackend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "포스트 생성 요청 DTO")
 public class PostCreateReqDto {
 
-    private final String email;
+    @Schema(description = "포스트 제목", example = "제목입니다")
     private final String title;
+
+    @Schema(description = "포스트 내용", example = "내용입니다")
     private final String content;
 }

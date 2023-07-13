dca07e74e7f7f74b02df4bfcf9e23be020c38bb2
soonhankwon <soonable@gmail.com>
2023-07-13 15:49:29 +0900

    feat : 유저 포스트 조회 res dto 스웨거 설명추가

--- src/main/java/edu/fivestar/fivestarbackend/dto/UserPostGetResDto.java
+++ src/main/java/edu/fivestar/fivestarbackend/dto/UserPostGetResDto.java
@@ -11,9 +11,18 @@ import java.time.LocalDateTime;
 @Schema(description = "유저 포스트 조회 응답 DTO")
 public class UserPostGetResDto {
 
+    @Schema(description = "포스트 아이디", example = "1")
     private final Long id;
+
+    @Schema(description = "포스트 제목", example = "협업이란?")
     private final String title;
+
+    @Schema(description = "포스트 내용", example = "바로 이것입니다.")
     private final String content;
+
+    @Schema(description = "포스트 생성일자", example = "2023-07-12T17:56:26.643536")
     private final LocalDateTime createAt;
+
+    @Schema(description = "포스트 수정일자", example = "2023-07-17T17:56:26.643536")
     private final LocalDateTime modifiedAt;
 }

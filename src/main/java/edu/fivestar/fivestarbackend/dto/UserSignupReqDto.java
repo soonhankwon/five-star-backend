a3da7e7d0a24dbdbba4588fcf3c00575b89c73ed
soonhankwon <soonable@gmail.com>
2023-07-13 15:46:43 +0900

    feat : 유저 가입 req dto 스웨거 설명추가

--- src/main/java/edu/fivestar/fivestarbackend/dto/UserSignupReqDto.java
+++ src/main/java/edu/fivestar/fivestarbackend/dto/UserSignupReqDto.java
@@ -1,15 +1,20 @@
 package edu.fivestar.fivestarbackend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "유저 회원가입 요청 DTO")
 public final class UserSignupReqDto {
 
+    @Schema(description = "유저 이메일", example = "test@test.com")
     private final String email;
 
+    @Schema(description = "유저 닉네임", example = "tester")
     private final String name;
 
+    @Schema(description = "유저 패스워드", example = "password")
     private final String password;
 }

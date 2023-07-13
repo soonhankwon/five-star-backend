e01679b51e758cd3b801429d303d05644584f9dd
soonhankwon <soonable@gmail.com>
2023-07-13 15:44:54 +0900

    feat : 유저 탈퇴 req dto 변수 삭제 및 스웨거 설명추가

--- src/main/java/edu/fivestar/fivestarbackend/dto/UserResignReqDto.java
+++ src/main/java/edu/fivestar/fivestarbackend/dto/UserResignReqDto.java
@@ -1,15 +1,17 @@
 package edu.fivestar.fivestarbackend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "유저 탈퇴 요청 DTO")
 public final class UserResignReqDto {
 
+    @Schema(description = "유저 이메일", example = "test@test.com")
     private final String email;
 
-    private final String name;
-
+    @Schema(description = "유저 패스워드", example = "password")
     private final String password;
 }

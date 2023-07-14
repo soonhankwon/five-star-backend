aa8e814b2a1d5f121c78c48cd20958edc0848426
soonhankwon <soonable@gmail.com>
2023-07-14 14:51:30 +0900

    config : swagger 관련 url 인터셉터 제외

--- src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
+++ src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
@@ -1,7 +1,9 @@
 package edu.fivestar.fivestarbackend.config;
 
+import edu.fivestar.fivestarbackend.web.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,5 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/users/signup", "/login", "/logout",
+                        "/css/**","/js/**", "/*.ico", "/error", "/swagger-ui/**", "/v3/api-docs/**");
     }
 }

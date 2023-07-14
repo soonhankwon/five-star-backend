c6720a6920d7485e3267472f7d7c2e8e83ec68ee
soonhankwon <soonable@gmail.com>
2023-07-14 12:22:22 +0900

    config : cors 설정

---
+++ src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
@@ -0,0 +1,18 @@
+package edu.fivestar.fivestarbackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${ec2.server.address}")
+    private String serverAddress;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(serverAddress);
+    }
+}

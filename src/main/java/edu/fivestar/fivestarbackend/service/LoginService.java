a90d933f42d858885de8ccb904a77dc338b7b5b2
soonhankwon <soonable@gmail.com>
2023-07-14 15:13:58 +0900

    feat : login service interface(추상화)

---
+++ src/main/java/edu/fivestar/fivestarbackend/service/LoginService.java
@@ -0,0 +1,11 @@
+package edu.fivestar.fivestarbackend.service;
+
+import edu.fivestar.fivestarbackend.dto.LoginReqDto;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface LoginService {
+
+    void login(LoginReqDto dto, HttpServletRequest request);
+    void logout(HttpServletRequest request);
+}

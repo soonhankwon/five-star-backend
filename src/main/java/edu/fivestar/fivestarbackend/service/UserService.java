458d43a1b662771997572beb4556f1c36492b81c
soonhankwon <soonable@gmail.com>
2023-07-14 16:50:46 +0900

    feat : 회원 탈퇴 추상화

--- src/main/java/edu/fivestar/fivestarbackend/service/UserService.java
+++ src/main/java/edu/fivestar/fivestarbackend/service/UserService.java
@@ -1,10 +1,11 @@
 package edu.fivestar.fivestarbackend.service;
 
+import edu.fivestar.fivestarbackend.domain.User;
 import edu.fivestar.fivestarbackend.dto.UserResignReqDto;
 import edu.fivestar.fivestarbackend.dto.UserSignupReqDto;
 
 public interface UserService {
     void signupUser(UserSignupReqDto dto);
 
-    void resignUser(UserResignReqDto dto);
+    void resignUser(UserResignReqDto dto, User loginUser);
 }

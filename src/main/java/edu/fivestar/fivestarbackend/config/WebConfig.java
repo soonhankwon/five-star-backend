e05142f0b1167a68ac0af6fe161d33ec893d411a
soonhankwon <soonable@gmail.com>
2023-07-14 15:11:50 +0900

    config : argument resolver 설정

--- src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
+++ src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
@@ -1,10 +1,14 @@
 package edu.fivestar.fivestarbackend.config;
 
+import edu.fivestar.fivestarbackend.web.argumentresolver.LoginUserArgumentResolver;
 import edu.fivestar.fivestarbackend.web.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -21,5 +25,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/users/signup", "/login", "/logout",
                         "/css/**","/js/**", "/*.ico", "/error", "/swagger-ui/**", "/v3/api-docs/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginUserArgumentResolver());
     }
 }

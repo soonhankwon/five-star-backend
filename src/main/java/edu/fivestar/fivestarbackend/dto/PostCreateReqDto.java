66092274d78d2292d84754a0ffec176627207215
soonhankwon <soonable@gmail.com>
2023-07-14 18:11:59 +0900

    refactor : dto final 로 변경

--- src/main/java/edu/fivestar/fivestarbackend/dto/PostCreateReqDto.java
+++ src/main/java/edu/fivestar/fivestarbackend/dto/PostCreateReqDto.java
@@ -7,11 +7,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 @Schema(description = "포스트 생성 요청 DTO")
-public class PostCreateReqDto {
+public final class PostCreateReqDto {
 
-    @Schema(description = "포스트 제목", example = "제목입니다")
+    @Schema(description = "포스트 제목", example = "자바란 무엇인가?")
     private final String title;
 
-    @Schema(description = "포스트 내용", example = "내용입니다")
+    @Schema(description = "포스트 내용", example = "객체지향언어이다")
     private final String content;
 }

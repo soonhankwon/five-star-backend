87627405958eedc6cd61ae3bf255e78faea4623c
soonhankwon <soonable@gmail.com>
2023-07-17 14:33:52 +0900

    refactor : api 상세 설명 및 디폴트 페이지네이션 설정 추가

--- src/main/java/edu/fivestar/fivestarbackend/controller/PostController.java
+++ src/main/java/edu/fivestar/fivestarbackend/controller/PostController.java
@@ -9,6 +9,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,9 +37,10 @@ public class PostController {
     }
 
     @GetMapping
-    @Operation(summary = "유저 포스트 전체 조회 API", description = "?page=0&size=10&sort=id,DESC' page, size 페이지네이션, sort 정렬이 가능합니다.")
+    @Operation(summary = "유저 포스트 전체 조회 API", description = "ex) ?page=0&size=10&sort=id,DESC' => page, size 페이지네이션, sort 정렬이 가능, query param size, sort 생략시 기본값 size 10, sort id,DESC")
     @ResponseStatus(HttpStatus.OK)
-    public List<UserPostGetResDto> getPostsByUser(HttpServletRequest request, Pageable pageable) {
+    public List<UserPostGetResDto> getPostsByUser(HttpServletRequest request,
+                                                  @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
         User loginUser = getLoginUserBySession(request);
         return postServiceImpl.getPostsByUser(loginUser, pageable);
     }
@@ -71,8 +74,8 @@ public class PostController {
     private User getLoginUserBySession(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         User loginUser = (User) session.getAttribute(SessionConst.LOGIN_USER);
-        if(loginUser == null) {
-            throw  new RuntimeException("session disconnected");
+        if (loginUser == null) {
+            throw new RuntimeException("session disconnected");
         }
         return loginUser;
     }

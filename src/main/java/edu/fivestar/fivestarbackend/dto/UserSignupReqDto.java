81e6b196803e70386b20e58bafd8a9d7bc2c1d1d
soonhankwon <soonable@gmail.com>
2023-07-18 14:45:37 +0900

    feat : 회원 가입시 입력 필드 empty check validation

--- src/main/java/edu/fivestar/fivestarbackend/dto/UserSignupReqDto.java
+++ src/main/java/edu/fivestar/fivestarbackend/dto/UserSignupReqDto.java
@@ -4,17 +4,22 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @AllArgsConstructor
 @Schema(description = "유저 회원가입 요청 DTO")
 public final class UserSignupReqDto {
 
+    @NotEmpty
     @Schema(description = "유저 이메일", example = "abc@test.com")
     private final String email;
 
+    @NotEmpty
     @Schema(description = "유저 닉네임", example = "test")
     private final String name;
 
+    @NotEmpty
     @Schema(description = "유저 패스워드", example = "1234")
     private final String password;
 }

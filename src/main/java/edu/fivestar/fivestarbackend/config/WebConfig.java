7c11faf0d57de91e84766cfc6e009963421613f6
soonhankwon <soonable@gmail.com>
2023-07-24 16:01:22 +0900

    fix : Preflight Request 관련 interceptor 수정

--- src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
+++ src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
@@ -13,8 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PATCH", "DELETE");
+                .allowedOrigins("*");
     }
 
     @Override
--- src/main/java/edu/fivestar/fivestarbackend/web/interceptor/LoginCheckInterceptor.java
+++ src/main/java/edu/fivestar/fivestarbackend/web/interceptor/LoginCheckInterceptor.java
@@ -2,12 +2,14 @@ package edu.fivestar.fivestarbackend.web.interceptor;
 
 import edu.fivestar.fivestarbackend.web.session.SessionConst;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Objects;
 
 @Slf4j
 public class LoginCheckInterceptor implements HandlerInterceptor {
@@ -15,6 +17,10 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
+
+        if (isPreflightRequest(request)) {
+            return true;
+        }
 
         log.info("인증 체크 인터셉터 실행 {}", requestURI);
         HttpSession session = request.getSession(false);
@@ -24,5 +30,25 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
             throw new HttpSessionRequiredException("session expired or login plz");
         }
         return true;
+    }
+
+    private boolean isPreflightRequest(HttpServletRequest request) {
+        return isOptions(request) && hasHeaders(request) && hasMethod(request) && hasOrigin(request);
+    }
+
+    private boolean isOptions(HttpServletRequest request) {
+        return request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.toString());
+    }
+
+    private boolean hasHeaders(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Access-Control-Request-Headers"));
+    }
+
+    private boolean hasMethod(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Access-Control-Request-Method"));
+    }
+
+    private boolean hasOrigin(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Origin"));
     }
 }

e6804b271824f7c4c6a00d0be76f32e02498ffef
soonhankwon <soonable@gmail.com>
2023-07-24 15:07:26 +0900

    fix : localhost 3000 cors

--- src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
+++ src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
@@ -11,6 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
         registry.addMapping("/**").allowedOrigins("*");
     }
 

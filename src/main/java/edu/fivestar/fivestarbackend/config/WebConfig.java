ea6622102a9e7e238e4bd535fb29464c8736bc11
soonhankwon <soonable@gmail.com>
2023-07-24 20:02:13 +0900

    fix : cors config

--- src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
+++ src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
@@ -14,7 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addMapping("/**")
                 .allowCredentials(true)
-                .allowedOrigins("*")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")

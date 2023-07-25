b553e4acbc61631a6f8d99a01081238932847ab8
soonhankwon <soonable@gmail.com>
2023-07-25 16:02:14 +0900

    feat : 모든 포스트 조회 (페이지네이션)

--- src/main/java/edu/fivestar/fivestarbackend/repository/PostRepository.java
+++ src/main/java/edu/fivestar/fivestarbackend/repository/PostRepository.java
@@ -2,14 +2,17 @@ package edu.fivestar.fivestarbackend.repository;
 
 import edu.fivestar.fivestarbackend.domain.Post;
 import edu.fivestar.fivestarbackend.domain.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findPostsByUser(User user, Pageable pageable);
+    @Override
+    Page<Post> findAll(Pageable pageable);
 
     Optional<Post> findPostByUserAndId(User user, Long postId);
 }

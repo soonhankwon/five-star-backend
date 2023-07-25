0057e9a1362abe4937a5806ac302ea8e4aa51b0a
soonhankwon <soonable@gmail.com>
2023-07-25 16:03:41 +0900

    feat : Repository 애노테이션

--- src/main/java/edu/fivestar/fivestarbackend/repository/UserRepository.java
+++ src/main/java/edu/fivestar/fivestarbackend/repository/UserRepository.java
@@ -2,10 +2,11 @@ package edu.fivestar.fivestarbackend.repository;
 
 import edu.fivestar.fivestarbackend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByEmail(String email);
     Optional<User> findUserByEmailAndPassword(String email, String password);
 }

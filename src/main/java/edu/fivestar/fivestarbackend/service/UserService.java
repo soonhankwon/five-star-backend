4d9ed0a440afd628d096ae333de857706ac57df2
soonhankwon <soonable@gmail.com>
2023-07-25 19:23:52 +0900

    refactor : changed line

--- src/main/java/edu/fivestar/fivestarbackend/service/UserService.java
+++ src/main/java/edu/fivestar/fivestarbackend/service/UserService.java
@@ -5,6 +5,7 @@ import edu.fivestar.fivestarbackend.dto.UserResignReqDto;
 import edu.fivestar.fivestarbackend.dto.UserSignupReqDto;
 
 public interface UserService {
+
     void signupUser(UserSignupReqDto dto);
 
     void resignUser(UserResignReqDto dto, User loginUser);

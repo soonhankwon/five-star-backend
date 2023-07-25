ef2c365273efb0f1445974c171095566fb0f9889
soonhankwon <soonable@gmail.com>
2023-07-25 16:15:34 +0900

    fix : /posts 의 Get 요청은 인터셉터 통과

--- src/main/java/edu/fivestar/fivestarbackend/web/interceptor/LoginCheckInterceptor.java
+++ src/main/java/edu/fivestar/fivestarbackend/web/interceptor/LoginCheckInterceptor.java
@@ -2,6 +2,7 @@ package edu.fivestar.fivestarbackend.web.interceptor;
 
 import edu.fivestar.fivestarbackend.web.session.SessionConst;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -15,6 +16,11 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
+
+        if(request.getMethod().equals(HttpMethod.GET.name()) && requestURI.contains("/posts")) {
+            log.info("GET METHOD 인증 체크 인터셉터 통과");
+            return true;
+        }
 
         log.info("인증 체크 인터셉터 실행 {}", requestURI);
         HttpSession session = request.getSession(false);

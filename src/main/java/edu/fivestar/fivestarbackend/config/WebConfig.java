b2d5cbf8291ee9863457f8327b4de474f685c012
soonhankwon <soonable@gmail.com>
2023-07-25 19:25:04 +0900

    config : PasswordEncoder config - BCryptPasswordEncoder 사용

--- src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
+++ src/main/java/edu/fivestar/fivestarbackend/config/WebConfig.java
@@ -1,13 +1,21 @@
 package edu.fivestar.fivestarbackend.config;
 
 import edu.fivestar.fivestarbackend.web.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {

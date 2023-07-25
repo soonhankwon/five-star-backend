a7529621dd963c807583c92d7ed71df3af81e8b8
soonhankwon <soonable@gmail.com>
2023-07-25 15:59:16 +0900

    feat : DTO example 추가

--- src/main/java/edu/fivestar/fivestarbackend/dto/LoginReqDto.java
+++ src/main/java/edu/fivestar/fivestarbackend/dto/LoginReqDto.java
@@ -12,8 +12,10 @@ import javax.validation.constraints.NotEmpty;
 public final class LoginReqDto {
 
     @NotEmpty
+    @Schema(description = "이메일", example = "abc@test.com")
     private final String email;
 
     @NotEmpty
+    @Schema(description = "패스워드", example = "1234")
     private final String password;
 }

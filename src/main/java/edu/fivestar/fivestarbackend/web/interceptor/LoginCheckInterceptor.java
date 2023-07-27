1106d57123a8ff2c4d831f50a081d0186466a42a
Shingyu Kang <rkdtlseb@naver.com>
2023-07-27 14:27:50 +0900

    refector: Session 로그인 - 로그아웃 - 다시 접속할 때를 위한 isRequestedSessionIDValid()처리

--- src/main/java/edu/fivestar/fivestarbackend/web/interceptor/LoginCheckInterceptor.java
+++ src/main/java/edu/fivestar/fivestarbackend/web/interceptor/LoginCheckInterceptor.java
@@ -30,7 +30,7 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
         log.info("인증 체크 인터셉터 실행 {}", requestURI);
         HttpSession session = request.getSession(false);
 
-        if(session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
+        if(session == null || !request.isRequestedSessionIdValid() || session.getAttribute(SessionConst.LOGIN_USER) == null) {
             log.info("미인증 사용자 요청");
             throw new HttpSessionRequiredException("session expired or login plz");
         }

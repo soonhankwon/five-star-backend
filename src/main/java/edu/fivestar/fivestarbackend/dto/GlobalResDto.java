52efedc6dc20ed6d2e319a3b4c7638dc92171549
soonhankwon <soonable@gmail.com>
2023-08-02 11:33:13 +0900

    feat : 단일 메세지 응답 DTO -> GlobalResDto 로 사용 및 해당 Api -> ApiResponse 추가

---
+++ src/main/java/edu/fivestar/fivestarbackend/dto/GlobalResDto.java
@@ -0,0 +1,11 @@
+package edu.fivestar.fivestarbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public final class GlobalResDto {
+
+    private final String message;
+}
